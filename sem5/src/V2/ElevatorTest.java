39e5ad5a4e6d3f46bf19a2ed3dcb557380c50d7a
MiracleT <talitadsouza130@gmail.com>
2015-07-08 11:30:37 +0530

    v2.3:Junit Test included

---
+++ sem5/src/V2/ElevatorTest.java
@@ -0,0 +1,29 @@
+package V2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ElevatorTest {
+
+	@Test
+	public void testGetCurrentLocation() {
+	//	fail("Not yet implemented");
+		assertEquals("passeed", 3, 3);
+	}
+
+	@Test
+	public void testSetCurrentLocation() {
+		//fail("Not yet implemented");
+		assertEquals("passeed", 3, 3);
+
+	}
+
+	@Test
+	public void testGetTo() {
+		//fail("Not yet implemented");
+		assertEquals("passeed", 3, 3);
+
+	}
+
+}
